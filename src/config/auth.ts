587e0484315158f5ee98e56dc1b7e14df92c84dd
Joao Macedo <joaovictormacedo91@gmail.com>
2020-08-27 17:42:51 -0300

    Initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '913accf6582aa383d3b7cd0a717a2af5',
+    expiresIn: '1d',
+  },
+};
---
+++ src/models/Appointment.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+/**
+ * Um para um (OneToOnte)
+ * Um para muitos (OnteToMany)
+ * Muitos para um (ManyToOne)
+ * Muitos para Muitos (ManyToMany)
+ */
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Appointment;
